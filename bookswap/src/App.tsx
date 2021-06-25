3934f81dc954be6b281496847477767efcad3dbb
bohdanmalyk <bmalyk@gmail.com>
2021-06-25 20:30:26 +0200

    fix react-router-dom

--- bookswap/src/App.tsx
+++ bookswap/src/App.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import '../src/styles/main.css';
+import Home from "./Home";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-        <div className="w-32 h-32 rounded-full bg-pink-500"></div>
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/home">Start page</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+
+                <Switch>
+                    <Route path="/home">
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     </div>
   );
 }
---
+++ bookswap/src/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return(
+        <>
+            <h1 className="text-xl text-red-300 font bold">Homepage</h1>
+        </>
+    )
+}
+
+export default Home;
