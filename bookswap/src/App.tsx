44a7d09f864c07f938637d1035b159bbe6159083
bohdanmalyk <bmalyk@gmail.com>
2021-06-25 16:14:23 +0200

    create and setup project

---
+++ bookswap/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../src/styles/main.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className="w-32 h-32 rounded-full bg-pink-500"></div>
+    </div>
+  );
+}
+
+export default App;
