cfe5f5b7624ba42937e5c11f1ded36fe19b0e412
bohdanmalyk <bmalyk@gmail.com>
2021-07-14 23:53:16 +0200

    add SearchInput

--- pages/getBooks.tsx
+++ pages/getBooks.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import {useQuery} from "urql";
+import withAuth from "../components/HOC";
 
 const GetBooksQuery =`
 query($search: String){
@@ -12,31 +13,27 @@ query($search: String){
 `
 
 const GetBooks = () => {
-  let id = 1;
 const [search, setSearch] = useState('');
   const [result,] = useQuery({
   query: GetBooksQuery,
   variables: {search: search},
 })
   const {data, fetching, error} = result;
-  if(fetching) return <h1>Loading...</h1>
-  if(error) return <h1>Opps something went wrong</h1>
-
-    const {books} = data.getBooks
     return (
       <div className=" px-5 py-3">
         <h1>Books</h1>
         <div>
           <input onChange={event => setSearch(event.target.value)} value={search} type='text' className="border w-64"/>
         </div>
-          <div className="flex flex-col">
-            {books.map((item: any, index: number)=>{
-              console.log(index)
-             return <span key={id=id+1}>{item.title}</span>
-            })}
-          </div>
+        { fetching ? <h1>Loading...</h1> : '' }
+        { error ? <h1>Opps something went wrong</h1> : '' }
+        <div className="flex flex-col">
+        {data?.getBooks.books ? data?.getBooks.books.map(( item: any, indx: number  ) => {
+          return <span key={indx}>{item.title}</span>
+        }) : ''}
+        </div>
       </div>
     )
 }
 
-export default GetBooks
+export default withAuth(GetBooks)
