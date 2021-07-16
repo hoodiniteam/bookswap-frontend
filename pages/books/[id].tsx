d63b0726765f069f34c303f3cc8a4d7835510ca6
bohdanmalyk <bmalyk@gmail.com>
2021-07-16 15:30:04 +0200

    add updateBook without status and conditional

--- pages/books/[id].tsx
+++ pages/books/[id].tsx
@@ -1,5 +1,5 @@
-import React, {FormEvent, useEffect} from "react";
-import { useQuery} from "urql";
+import React, {ChangeEvent, FormEvent, useEffect, useState} from "react";
+import {useMutation, useQuery} from "urql";
 import {useRouter} from "next/router";
 const GetBook = `
 query($id:String!){
@@ -14,86 +14,83 @@ query($id:String!){
   }
 }
 `
-// const UpdateBookMutation = `
-// mutation($id:String!, $title:String, $description: String, $image: String, $status:BooksStatus){
-//   updateBook(options:{id:$id, title:$title, description:$description, image: $image, status:$status}){
-// \t\t    status
-//   }
-// }
-// `
-
-// enum BooksStatus {
-//   DELIVERING,
-//   EXTRACTED,
-//   HOLD,
-//   OPEN
-// }
-//
-// enum BooksCondition {
-//   BRANDNEW,
-//   LIKENEW,
-//   GOOD,
-//   SATISFACTORY,
-//   BAD,
-//   TERRIBLE,
-// }
-//
-// type BookData = {
-//   condition: BooksCondition
-//   description: string
-//   id: string
-//   image: string
-//   status: BooksStatus
-//   title: string
-// }
+const UpdateBookMutation = `
+mutation($id:String!, $title:String, $description: String, $image: String, $status:BooksStatus){
+  updateBook(options:{id:$id, title:$title, description:$description, image: $image, status:$status}){
+    status
+    errors {
+      message
+      field
+    }
+  }
+}
+`
+type BookData = {
+  description: string
+  id: string
+  image: string
+  title: string
+}
 
 const Book = () => {
-  // const [book, setBook] = useState<BookData | null>(null)
   const router = useRouter();
   const [result,] = useQuery({
     query: GetBook,
     variables: {id: router.query.id}
   });
-  // const [,updateBook] = useMutation(UpdateBookMutation)
-
-  const {data, fetching, error} = result;
+  const [,updateBook] = useMutation(UpdateBookMutation)
+  const [book, setBook] = useState<BookData | null>(null)
   useEffect(() => {
-  }, [])
-  const onChangeHandler = (e: FormEvent<HTMLInputElement | HTMLSelectElement>) =>{
-    console.log(e.target);
+    if(result.data){
+      setBook(result.data.getBook.book)
+    }
+  }, [result])
+  if (result.fetching) return <p>Loading...</p>;
+  if (result.error) return <p>Oh no... {result.error.message}</p>;
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) =>{
+    const {name, value} = e.target;
+    if(book){
+      setBook({...book, [name]: value})
+    }
   }
-  if (fetching) return <p>Loading...</p>;
-  if (error) return <p>Oh no... {error.message}</p>;
-  return (
-    <>
-      <form className="flex flex-col border-2 p-5 max-w-min">
-        <div className='flex justify-between my-1.5'>
-          Title:
-          <input className="border" name="title" defaultValue={data.updateBook.book.title} onChange={onChangeHandler}/>
-        </div>
-        <div className='flex justify-between my-1.5'>
-          Description:
-          <input className="border" name="description" defaultValue={data.updateBook.book.description} onChange={onChangeHandler}/>
-        </div>
-        <div className='flex justify-between my-1.5'>
-          Image:
-          <input className="border" name="image" defaultValue={data.updateBook.book.image} onChange={onChangeHandler}/>
-        </div>
-        <div className='flex justify-between my-1.5'>
-          Title:
-          <select defaultValue={data.updateBook.book.condition} name="condition" className="border w-44" onChange={onChangeHandler}>
-            <option>BRANDNEW</option>
-            <option>LIKENEW</option>
-            <option>GOOD</option>
-            <option>SATISFACTORY</option>
-            <option>BAD</option>
-            <option>TERRIBLE</option>
-          </select>
-        </div>
-        <button type="submit">Change</button>
-      </form>
-      <button onClick={() => router.push('/getBooks')}>Previous</button>
-    </>
-  )
+  const onSubmitHandler = (e: FormEvent<HTMLElement>) => {
+    e.preventDefault();
+    if(book){
+      const variables = {
+        id: router.query.id,
+        title: book.title,
+        description: book.description,
+        image: book.image,
+      }
+      updateBook(variables).then(res=>{
+        console.log(res);
+      })
+    }
+
+  }
+  if(!result.fetching && book !== null){
+    return (
+      <>
+        <form className="flex flex-col border-2 p-5 max-w-min" onSubmit={onSubmitHandler}>
+          <div className='flex justify-between my-1.5'>
+            Title:
+            <input className="border" name="title" defaultValue={book.title} onChange={onChangeHandler}/>
+          </div>
+          <div className='flex justify-between my-1.5'>
+            Description:
+            <input className="border" name="description" defaultValue={book.description} onChange={onChangeHandler}/>
+          </div>
+          <div className='flex justify-between my-1.5'>
+            Image:
+            <input className="border" name="image" defaultValue={book.image} onChange={onChangeHandler}/>
+          </div>
+          <button type="submit">Change</button>
+        </form>
+        <button onClick={() => router.push('/getBooks')}>Previous</button>
+      </>
+    )
+  }
+  return null;
+
 }
 export default Book
