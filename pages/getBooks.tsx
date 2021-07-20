473b43471bf7eaef33d28ac2d42f8801ddf3981d
bohdanmalyk <bmalyk@gmail.com>
2021-07-20 22:11:21 +0200

    fix pagination

--- pages/getBooks.tsx
+++ pages/getBooks.tsx
@@ -5,8 +5,9 @@ import {useRouter} from "next/router";
 import Pagination from "../components/pagination";
 
 const GetBooksQuery =`
-query($search: String){
-  getBooks(search: $search){
+query($search: String, $status: BooksStatus, $offset: Float, $limit: Float){
+  getBooks(search: $search, status: $status, offset: $offset, limit: $limit){
+    count
     books{
       title
       id
@@ -14,26 +15,27 @@ query($search: String){
   }
 }
 `
-
 const GetBooks = () => {
   const [search, setSearch] = useState('');
+  const [total, setTotal] = useState(0)
   const [books, setBooks] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [booksPerPage] = useState(10);
   const router = useRouter()
   const [result,] = useQuery({
   query: GetBooksQuery,
-  variables: {search: search},
+  variables: {search: search, limit:booksPerPage, offset: currentPage}
 })
   useEffect(() => {
     if(result.data){
       setBooks(result.data.getBooks.books)
+      setTotal(result.data.getBooks.count)
     }
   }, [result])
-  const indexOfLastBook = currentPage * booksPerPage;
-  const indexOfFirstBook = indexOfLastBook - booksPerPage;
-  const currentBooks = books.slice(indexOfFirstBook, indexOfLastBook);
-  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+  const paginate = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  }
+
   const onClickHandler = (e: {target: any}) => {
     router.push(`/books/${e.target?.id}`)
   }
@@ -46,11 +48,13 @@ const GetBooks = () => {
         { result.fetching ? <h1>Loading...</h1> : '' }
         { result.error ? <h1>Opps something went wrong</h1> : '' }
         <div className="flex flex-col">
-          {!result.fetching && books !== null ? currentBooks.map(( item: any, indx: number  ) => {
+          {!result.fetching && books !== null ? books.map(( item: any, indx: number  ) => {
             return <span className="cursor-pointer" onClick={onClickHandler} id={item.id} key={indx}>{item.title}</span>
           }) : null}
         </div>
-          <Pagination booksPerPage={booksPerPage} totalBooks={books.length} paginate={paginate} />
+          <ul className="flex justify-between w-20">
+           <Pagination booksPerPage={booksPerPage} totalBooks={total} paginate={paginate} />
+          </ul>
       </div>
     )
 }
