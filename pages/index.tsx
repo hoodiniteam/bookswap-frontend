d419c6d2eba47597d908ba0e93a0869581e53183
bohdanmalyk <bmalyk@gmail.com>
2021-07-27 13:10:33 +0200

    add redirect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {useRouter} from "next/router";
 
 export default function Index() {
+    const router = useRouter()
+    useEffect(()=>{
+      router.push('/home')
+    },[])
   return (
     <div>
-      <h1>Index</h1>
     </div>
   )
 }
