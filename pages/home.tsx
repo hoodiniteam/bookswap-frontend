6c249a9a11eb079767c3466499c23ddf67fa4b4f
bohdanmalyk <bmalyk@gmail.com>
2021-07-28 22:53:01 +0200

    add layout to HOC

--- pages/home.tsx
+++ pages/home.tsx
@@ -13,18 +13,13 @@ const Home = () => {
               onClick={logOut}>LogOut
       </button>
       <h1>HomePage</h1>
-      <div className="flex justify-between items-center w-80 mt-5 px-5">
-          <button className="px-1.5 py-0.5 text-white bg-blue-400 rounded-full"
+      <div className="flex justify-between items-center w-64 mt-5 px-5">
+        <button className="px-1.5 py-0.5 text-white bg-blue-400 rounded-full"
                 onClick={() => router.push('/createBook')}>Add book
-          </button>
-          <button className="px-1.5 py-0.5 text-white bg-blue-400 rounded-full"
-                onClick={() => router.push('/profile')}>
-                About me
-          </button>
-          <button className="px-1.5 py-0.5 text-white bg-blue-400 rounded-full"
-                  onClick={() => router.push('/books')}>
-                    Books
-          </button>
+        </button>
+        <button className="px-1.5 py-0.5 text-white bg-blue-400 rounded-full" onClick={() => router.push('/profile')}>About
+          me
+        </button>
       </div>
     </>
   )
