95faa2dd6d90fb421a9738f0c51fbb4505063043
bohdanmalyk <bmalyk@gmail.com>
2021-08-05 12:47:21 +0200

    fix LogOut function

--- helpers/LogOut.ts
+++ helpers/LogOut.ts
@@ -1,5 +1,5 @@
 const LogOut = () => {
-    window.location.href = 'http://localhost:3000/books/';
+    window.location.href = 'http://localhost:3000/login/';
     localStorage.clear()
 }
 export default LogOut
