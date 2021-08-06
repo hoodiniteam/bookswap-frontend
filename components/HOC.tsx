1d3b20b3d196daad22990d2ec6ed756f4f52a301
bohdanmalyk <bmalyk@gmail.com>
2021-08-06 10:38:09 +0200

    #33 fix withAuth

--- components/HOC.tsx
+++ components/HOC.tsx
@@ -5,22 +5,20 @@ import Layout from "./layout";
 const withAuth = (WrappedComponent: React.ComponentType) => {
   // eslint-disable-next-line react/display-name
   return (props: any) => {
-    if (typeof window !== "undefined") {
-      const Router = useRouter();
-      // @ts-ignore
+    if(typeof window !== "undefined"){
+      const Router = useRouter()
       const accessToken = localStorage.getItem("token");
-      if (!accessToken) {
-        Router.replace("/login").then();
-        return null;
-      }
-      return (
-          <Layout>
-            <WrappedComponent {...props} />
-          </Layout>
-      )
+        if (!accessToken) {
+          Router.replace("/login").then();
+          return null;
+        }
     }
-    return null;
-  };
+      return (
+        <Layout>
+          <WrappedComponent {...props} />
+        </Layout>
+      )
+  }
 };
 
 export default withAuth
