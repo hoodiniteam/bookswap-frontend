3a9f36731d0542c302307e4d660b47ea0bb1ae22
bohdanmalyk <bmalyk@gmail.com>
2021-09-20 22:58:08 +0200

    #59 remove button

--- pages/books/[id]/index.tsx
+++ pages/books/[id]/index.tsx
@@ -164,13 +164,6 @@ const Book = ({title}: any) => {
                 <Head>
                     <title>{book.title}</title>
                 </Head>
-                <button
-                    type='button'
-                    className='inline-flex m-5 items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-                    onClick={() => router.back()}
-                >
-                    Previous
-                </button>
                 {book.creator.id === userId ?
                     <button
                         onClick={() => router.push(`${router.asPath}/change`)}
