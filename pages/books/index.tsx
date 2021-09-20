7a103b80748dd1fa7cc2fd029256d6dca2893836
damirJa <zs.keda@gmail.com>
2021-09-20 12:58:08 +0500

    remove search

--- pages/books/index.tsx
+++ pages/books/index.tsx
@@ -105,26 +105,7 @@ const Index = () => {
   if(books !== null){
       return (
         <>
-          <div className="col-span-2 flex justify-between">
-          <div className="max-w-lg w-full lg:max-w-xs">
-              <label htmlFor="search" className="sr-only">
-                Search
-              </label>
-              <div className="relative text-gray-400 focus-within:text-gray-600">
-                  <div className="pointer-events-none absolute inset-y-0 left-0 pl-3 flex items-center">
-                    <SearchIcon className="h-5 w-5" aria-hidden="true" />
-                   </div>
-                  <input
-                      onChange={onHandlerSearch}
-                      value={searchTerm}
-                      id="search"
-                      className="block w-full bg-white py-2 pl-10 pr-3 border border-gray-400 rounded-md leading-5 text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-600 focus:ring-white focus:border-white sm:text-sm"
-                      placeholder="Search"
-                      type="search"
-                      name="search"
-                  />
-              </div>
-          </div>
+          <div className="col-span-2 flex flex-row-reverse">
             <select name="status"
                     value={status}
                     className="block bg-white py-2 px-3 border border-gray-400 rounded-md leading-5 text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-600 focus:ring-white focus:border-white sm:text-sm"
