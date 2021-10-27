95e1e1351e6fae887bcefc25294bed86606662f2
Damir Sadykov <zs.keda@gmail.com>
2021-10-27 11:17:58 +0500

    add env variable

--- helpers/staticRequest.ts
+++ helpers/staticRequest.ts
@@ -1,15 +1,16 @@
+require('dotenv').config();
 import axios from "axios";
 import { GetStaticEditions } from "../graphql/GetStaticEditions";
 import { GetStaticBooks } from "../graphql/GetStaticBooks";
 
 export const getStaticEditions = async () => {
-    return axios.post < { data: { getEditionsStatic: string[] } }>('http://localhost:4000/graphql', {
+    return axios.post < { data: { getEditionsStatic: string[] } }>(process.env.API_URL || '', {
         query: GetStaticEditions
     })
 };
 
 export const getStaticBooks = async () => {
-    return axios.post < { data: { getBooksStatic: string[] } }>('http://localhost:4000/graphql', {
+    return axios.post < { data: { getBooksStatic: string[] } }>(process.env.API_URL || '', {
         query: GetStaticBooks
     })
 };
--- src/UrqlClient.ts
+++ src/UrqlClient.ts
@@ -1,12 +1,13 @@
+require('dotenv').config();
 import {createClient} from 'urql';
 import Cookies from "js-cookie";
 
 const isServerSide = typeof window === 'undefined';
 
 const client = createClient({
-  url: 'https://bookswap-api-2-627cm.ondigitalocean.app/graphql',
+  // url: 'https://bookswap-api-2-627cm.ondigitalocean.app/graphql',
   // url: 'https://jolly-termite-53.loca.lt/graphql',
-  // url: 'http://localhost:4000/graphql',
+  url: process.env.API_URL || '',
   fetchOptions: () => {
     if (!isServerSide){
       const token = Cookies.get('token');
