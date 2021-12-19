172ca8a82b4cbb773f9e98f61c11b1ce730addf9
Damir Sadykov <zs.keda@gmail.com>
2021-12-19 15:25:16 +0500

    fix book creation

--- graphql/CreateBookMutation.ts
+++ graphql/CreateBookMutation.ts
@@ -1,8 +1,8 @@
 import {fragmentBook} from "./fragmentBook";
 
 export const CreateBookMutation = `
-mutation($title: String!, $description: String!, $userDescription: String!, $image: String, $condition:BooksCondition!, $isbn_10: String, $isbn_13: String ){
-  createBook(options:{title: $title, description: $description, userDescription: $userDescription, image: $image, condition: $condition, isbn_10: $isbn_10, isbn_13: $isbn_13 }){
+mutation($title: String!, $description: String!, $userDescription: String!, $image: String, $condition:BooksCondition!, $isbn_10: String, $isbn_13: String, $authors: [String!]!, $publishedDate: String ){
+  createBook(options:{title: $title, description: $description, userDescription: $userDescription, image: $image, condition: $condition, isbn_10: $isbn_10, isbn_13: $isbn_13, authors: $authors, publishedDate: $publishedDate }){
     status
     ${fragmentBook}
   }
